51d02170e4ebc214a595d5b0175ae23cba4d17cd
shivadestoryer <shvmprasad@gmail.com>
2025-02-23 12:55:15 +0530

    added categoryRepo

---
+++ src/main/java/com/scaler/capstoneproject/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.scaler.capstoneproject.repository;
+
+import com.scaler.capstoneproject.models.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+}
